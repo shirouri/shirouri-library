fa0d58cae47817210ab215be89585b4c84d688ee
hasitai <t3m2jskedy1@outlook.jp>
2024-11-18 18:17:50 +0900

    add counter

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// alert("このページはとても安全だよ！")
+function count(){
+    var thisCount = $("#count").html();
+        thisCount = Number(thisCount);
+        thisCount = thisCount + 1;
+    $("#count").html(thisCount);
+  }
