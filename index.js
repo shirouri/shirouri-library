1bebc5cd487000a7cf8af910187965c0ab7ee011
hasitai <t3m2jskedy1@outlook.jp>
2024-11-19 16:14:25 +0900

    add hamburger

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function count(){
+//チェックボックスのチェックを外す
+$(function () {
+    $('.burger_nav_item>a').click(function () {
+        $('#drawer_input').removeAttr('checked').prop('checked', false).change();
+    });
+});
+
+
+function count() {
     var thisCount = $("#count").html();
-        thisCount = Number(thisCount);
-        thisCount = thisCount + 1;
+    thisCount = Number(thisCount);
+    thisCount = thisCount + 1;
     $("#count").html(thisCount);
-  }
+}
